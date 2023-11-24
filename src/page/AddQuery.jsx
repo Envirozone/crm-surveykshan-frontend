999f1ed44644c9390708452ab9ed62966fbe7062
pc <Envirozone>
2023-11-24 10:39:41 +0530

    Fix Bug of Add Query Page, Added New Font Style

--- src/page/AddQuery.jsx
+++ src/page/AddQuery.jsx
@@ -185,7 +185,7 @@ function AddQuery() {
                 id="bordered-radio-2"
                 type="radio"
                 value={other}
-                // onChange={() => setOther(true)}
+                onChange={() => setTitle("")}
                 name="bordered-radio"
                 className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
               />
