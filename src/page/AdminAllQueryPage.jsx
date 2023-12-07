aca2a155c9adae4f1799ba54f116c50ee065f752
pc <Envirozone>
2023-12-07 16:26:14 +0530

    Update UI of All Query Page

--- src/page/AdminAllQueryPage.jsx
+++ src/page/AdminAllQueryPage.jsx
@@ -222,9 +222,7 @@ function AdminAllQueryPage() {
                 Token
               </th>
               <th scope="col" className="px-6 py-3">
-                <span className="material-symbols-outlined text-blue-600 text-2xl">
-                  mark_unread_chat_alt
-                </span>
+                Chats
               </th>
               <th scope="col" className="px-6 py-3">
                 User Name
@@ -241,11 +239,11 @@ function AdminAllQueryPage() {
               <th scope="col" className="px-6 py-3">
                 Query Title
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 text-center">
                 Status
               </th>
               <th scope="col" className="px-6 py-3">
-                Edit
+                Details
               </th>
               <th scope="col" className="px-6 py-3">
                 Delete
@@ -258,11 +256,12 @@ function AdminAllQueryPage() {
                 return (
                   <tr
                     key={query._id}
-                    className="bg-white border-b   hover:bg-gray-50 "
+                    className="bg-white border-b hover:bg-gray-50 "
                   >
                     <th
                       scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                      className="px-6 py-4 font-medium text-gray-900"
+                      style={{ width: "121px" }}
                     >
                       {`${query.createdAt.split("T")[0]} | ${
                         query.createdAt.split("T")[1].split("+")[0]
@@ -282,12 +281,28 @@ function AdminAllQueryPage() {
                         const [ticketId, counts] = item.split("+");
                         if (ticketId === query._id) {
                           return (
-                            <div
-                              key={ticketId}
-                              className="p-2 rounded-full bg-red-300 text-center font-bold border"
-                            >
-                              {counts}
-                            </div>
+                            <Link to={`/all-queries/${query._id}`}>
+                              <button
+                                key={ticketId}
+                                type="button"
+                                class="relative inline-flex items-center p-1.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                              >
+                                <svg
+                                  class="w-5 h-5"
+                                  aria-hidden="true"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="currentColor"
+                                  viewBox="0 0 20 16"
+                                >
+                                  <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                                  <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                                </svg>
+                                <span class="sr-only">Notifications</span>
+                                <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
+                                  {counts}
+                                </div>
+                              </button>
+                            </Link>
                           );
                         }
                       })}
@@ -296,49 +311,79 @@ function AdminAllQueryPage() {
                     <td className="px-6 py-4">{query.industry_name}</td>
                     <td className="px-6 py-4">{query.industry_partner}</td>
                     <td className="px-6 py-4">{query.phone}</td>
-                    <td className="px-6 py-4">{query.query_title}</td>
-                    <td
-                      style={
-                        query.status === "pending"
-                          ? {
-                              textDecoration: "underline",
-                              textDecorationColor: "red",
-                              textDecorationThickness: "0.3em",
-                            }
-                          : query.status === "inProgress"
-                          ? {
-                              textDecoration: "underline",
-                              textDecorationColor: "yellow",
-                              textDecorationThickness: "0.3em",
-                            }
-                          : {
-                              textDecoration: "underline",
-                              textDecorationColor: "green",
-                              textDecorationThickness: "0.3em",
-                            }
-                      }
-                      className="px-6 py-4"
-                    >
-                      {query.status === "inProgress"
-                        ? "In Progress"
-                        : query.status === "pending"
-                        ? "Pending"
-                        : query.status === "resolved"
-                        ? "Resolved"
-                        : ""}
+                    <td className="px-6 py-4">
+                      <p
+                        style={{
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          width: "180px",
+                        }}
+                      >
+                        {query.query_title}
+                      </p>
+                    </td>
+                    <td className="px-6 py-4 text-center w-40">
+                      <p
+                        style={
+                          query.status === "pending"
+                            ? {
+                                textDecoration: "underline",
+                                textDecorationColor: "red",
+                                backgroundColor: "red",
+                                textDecorationLine: "none",
+                                padding: "10px",
+                                color: "white",
+                                borderRadius: "5px",
+                                // textDecorationThickness: "0.3em",
+                              }
+                            : query.status === "inProgress"
+                            ? {
+                                textDecoration: "underline",
+                                textDecorationColor: "yellow",
+                                backgroundColor: "#FFD800",
+                                textDecorationLine: "none",
+                                padding: "10px",
+                                color: "white",
+                                borderRadius: "5px",
+                                // textDecorationThickness: "0.3em",
+                              }
+                            : {
+                                textDecoration: "underline",
+                                textDecorationColor: "green",
+                                backgroundColor: "green",
+                                textDecorationLine: "none",
+                                padding: "10px",
+                                color: "white",
+                                borderRadius: "5px",
+                                // textDecorationThickness: "0.3em",
+                              }
+                        }
+                      >
+                        {query.status === "inProgress"
+                          ? "In Progress"
+                          : query.status === "pending"
+                          ? "Pending"
+                          : query.status === "resolved"
+                          ? "Resolved"
+                          : ""}
+                      </p>
                     </td>
 
-                    <td className="px-6 py-4 text-left">
+                    <td className="px-6 py-4 text-center">
                       <Link
                         to={`/all-queries/${query._id}`}
                         className="font-medium text-blue-600  hover:underline"
                       >
-                        More Info
+                        <span class="material-symbols-outlined text-blue-500 font-bold-100 text-4xl">
+                          quick_reference
+                        </span>
+                        {/* More Info  */}
                       </Link>
                     </td>
-                    <td className="px-6 py-4 text-left ">
+                    <td className="px-6 py-4 text-center">
                       <button onClick={() => handleDeleteTicket(query._id)}>
-                        <span className="material-symbols-outlined text-red-500 font-bold-100">
+                        <span className="material-symbols-outlined text-red-500 font-bold-100 text-4xl">
                           delete
                         </span>
                       </button>
